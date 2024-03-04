8e1923bc5051ebed01f406dba429354c201a5fc5
ikeemwilson <IWilson@ECFMG.ORG>
2024-03-04 15:59:56 -0500

    Add constructor and hear method to Person class

--- classes/shouty.js
+++ classes/shouty.js
@@ -1,10 +1,17 @@
 class Person {
-  moveTo(distance) {
+  constructor(network) {
+    this.messages = []
+    this.network = network
 
+    //this.network.subscribe(this)
   }
 
   shout(message) {
+    //this.network.broadcast(message)
+  }
 
+  hear(message) {
+    this.messages.push(message)
   }
 
   messagesHeard(message) {
