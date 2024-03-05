ec8ea96eb03e693f8385cf1ca0f3da92ed6268b9
ikeemwilson <IWilson@ECFMG.ORG>
2024-03-05 09:36:46 -0500

    Loops: Removing redundant scenarios

--- classes/shouty.js
+++ classes/shouty.js
@@ -1,26 +1,41 @@
 class Person {
-  constructor(network) {
+  constructor(network, location) {
     this.messages = []
     this.network = network
+    this.location = location
 
-    //this.network.subscribe(this)
+    this.network.subscribe(this)
   }
 
   shout(message) {
-    //this.network.broadcast(message)
+    this.network.broadcast(message, this.location)
   }
 
   hear(message) {
     this.messages.push(message)
   }
 
-  messagesHeard(message) {
-    return [message]
+  messagesHeard() {
+    return this.messages
   }
 }
 
 class Network {
+  constructor(range) {
+    this.listeners = []
+    this.range = range
+  }
 
+  subscribe(person) {
+    this.listeners.push(person)
+  }
+
+  broadcast(message, shouter_location) {
+    this.listeners.forEach(listener => {
+      if (Math.abs(listener.location - shouter_location) <= this.range)
+        listener.hear(message)
+    })
+  }
 }
 
 export { Person, Network };
--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,9 +1,8 @@
 import { Person, Network } from '../../classes/shouty.js';
 import { Given, When, Then, Before } from '@cucumber/cucumber';
-import { assertThat, is } from 'hamjest';
+import { assertThat, contains, not } from 'hamjest';
 
 Before(function () {
-  this.network = new Network()
   this.people = {}
 });
 
@@ -13,10 +12,18 @@ Before(function () {
 //   this.sean = new Person
 //   this.lucy.moveTo(distance)
 // });
+
+Given('the range is {int}', function (range) {
+  this.network = new Network(range)
+})
  
-Given('a person named {word}', function (name) {
-  this.people[name] = new Person(this.network)
-});
+// Given('a person named {word}', function (name) {
+//   this.people[name] = new Person(this.network)
+// })
+
+Given('a person named {word} is located at {int}', function (name, location) {
+  this.people[name] = new Person(this.network, location)
+})
        
 When('Sean shouts {string}', function (message) {
   this.people['Sean'].shout(message)
@@ -24,5 +31,9 @@ When('Sean shouts {string}', function (message) {
 });
        
 Then('Lucy should hear Sean\'s message', function () {
-  assertThat(this.people['Lucy'].messagesHeard(this.messageFromSean), is([this.messageFromSean]))
+  assertThat(this.people['Lucy'].messagesHeard(this.messageFromSean), contains(this.messageFromSean))
+});
+
+Then('Larry should not hear Sean\'s message', function () {
+  assertThat(this.people['Larry'].messagesHeard(this.messageFromSean), not(contains(this.messageFromSean)))
 });
