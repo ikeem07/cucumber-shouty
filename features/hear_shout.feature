f682a4b578ce720e082d548840094cff0ff91a17
ikeemwilson <IWilson@ECFMG.ORG>
2024-03-05 09:57:49 -0500

    Loops: Incidental details

--- features/hear_shout.feature
+++ features/hear_shout.feature
@@ -8,10 +8,16 @@ Feature: Shout
   Rule: Shouts can be heard by other users
 
     Scenario: Listener hears a message
+      Given a person named Sean
+      And a person named Lucy
+      When Sean shouts "free bagels at Sean's"
+      Then Lucy should hear Sean's message
+
+    Scenario: Listener hears a different message
       Given the range is 100
       And a person named Sean is located at 0
       And a person named Lucy is located at 50
-      When Sean shouts "free bagels at Sean's"
+      When Sean shouts "Free coffee!"
       Then Lucy should hear Sean's message
 
   Rule: Shouts should only be heard if listener is within range
@@ -20,12 +26,12 @@ Feature: Shout
       Given the range is 100
       And a person named Sean is located at 0
       And a person named Lucy is located at 50
-      When Sean shouts "free bagels at Sean's"
-      Then Lucy should hear Sean's message
+      When Sean shouts
+      Then Lucy should hear a shout
 
     Scenario: Listener is out of range
       Given the range is 100
       And a person named Sean is located at 0
       And a person named Larry is located at 150
-      When Sean shouts "free bagels at Sean's"
-      Then Larry should not hear Sean's message
+      When Sean shouts
+      Then Larry should not hear a shout
--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,9 +1,12 @@
 import { Person, Network } from '../../classes/shouty.js';
 import { Given, When, Then, Before } from '@cucumber/cucumber';
-import { assertThat, contains, not } from 'hamjest';
+import { assertThat, contains, not, is } from 'hamjest';
+
+const default_range = 100
 
 Before(function () {
   this.people = {}
+  this.network = new Network(default_range)
 });
 
 //Given(/^Lucy is located (\d+) meter?s from Sean$/, function (distance) {
@@ -17,21 +20,33 @@ Given('the range is {int}', function (range) {
   this.network = new Network(range)
 })
  
-// Given('a person named {word}', function (name) {
-//   this.people[name] = new Person(this.network)
-// })
+Given('a person named {word}', function (name) {
+  this.people[name] = new Person(this.network, 0)
+})
 
 Given('a person named {word} is located at {int}', function (name, location) {
   this.people[name] = new Person(this.network, location)
+})
+
+When('Sean shouts', function () {
+  this.people['Sean'].shout('Hello, world')
 })
        
 When('Sean shouts {string}', function (message) {
   this.people['Sean'].shout(message)
   this.messageFromSean = message
 });
+
+Then('Lucy should hear a shout', function () {
+  assertThat(this.people['Lucy'].messagesHeard().length, is(1))
+});
        
 Then('Lucy should hear Sean\'s message', function () {
   assertThat(this.people['Lucy'].messagesHeard(this.messageFromSean), contains(this.messageFromSean))
+});
+
+Then('Larry should not hear a shout', function () {
+  assertThat(this.people['Larry'].messagesHeard(), not(contains(this.messageFromSean)))
 });
 
 Then('Larry should not hear Sean\'s message', function () {
