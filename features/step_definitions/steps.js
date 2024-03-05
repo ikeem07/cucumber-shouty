0a08df4358936ec762663b45b45fd15c50d49af8
ikeemwilson <IWilson@ECFMG.ORG>
2024-03-05 13:11:10 -0500

    Loops: DocString

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,6 +1,7 @@
 import { Person, Network } from '../../classes/shouty.js';
 import { Given, When, Then, Before } from '@cucumber/cucumber';
 import { assertThat, contains, not, is } from 'hamjest';
+import assert from 'assert';
 
 const default_range = 100
 
@@ -25,7 +26,7 @@ Given('a person named {word}', function (name) {
 })
 
 Given('people are located at', function (dataTable) {
-  dataTable.hashes().map((person) => {
+  dataTable.transpose().hashes().map((person) => {
     this.people[person.name] = new Person(this.network, person.location)
   })
 });
@@ -39,6 +40,11 @@ When('Sean shouts {string}', function (message) {
   this.messageFromSean = message
 });
 
+When('Sean shouts the following message', function (message) {
+  this.people['Sean'].shout(message)
+  this.messageFromSean = message
+})
+
 Then('Lucy should hear a shout', function () {
   assertThat(this.people['Lucy'].messagesHeard().length, is(1))
 });
@@ -47,10 +53,16 @@ Then('Lucy should hear Sean\'s message', function () {
   assertThat(this.people['Lucy'].messagesHeard(this.messageFromSean), contains(this.messageFromSean))
 });
 
-Then('Larry should not hear a shout', function () {
-  assertThat(this.people['Larry'].messagesHeard(), not(contains(this.messageFromSean)))
+Then('{word} should not hear a shout', function (name) {
+  assertThat(this.people[name].messagesHeard(), not(contains(this.messageFromSean)))
 });
 
 Then('Larry should not hear Sean\'s message', function () {
   assertThat(this.people['Larry'].messagesHeard(this.messageFromSean), not(contains(this.messageFromSean)))
+});
+
+Then('Lucy hears the following messages:', function (expectedMessages) {
+  let actualMessages = this.people['Lucy'].messagesHeard().map(message => [message])
+
+  assert.deepEqual(actualMessages, expectedMessages.raw())
 });
